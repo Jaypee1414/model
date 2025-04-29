99d2ed87891dec6a44cbc353471a1b182850d5be
jaypee1414 <jhnplbncyrn03@gmail.com>
2025-04-29 13:08:16 +0800

    add runtime

--- main.py
+++ main.py
@@ -93,5 +93,5 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # fallback to 8000 locally
-    uvicorn.run("server:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
